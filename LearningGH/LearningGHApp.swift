1b7024283c5d1d56ec3d206d4ef905784671ba67
Chaney Rangdel <chaneyrangdelwork@gmail.com>
2021-08-05 19:15:50 +0600

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Chaney Rangdel on 5/8/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
